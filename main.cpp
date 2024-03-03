346a08b0166479b4cbca45114913ae2457e0997e
katyacher <katyacher@inbox.ru>
2024-03-03 20:26:01 +0300

    exception added: std::invalid_argument() with std::cin in input() function

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ T average(const std::vector<T> &vec){
     static_assert( std::is_same<T, int>::value || std::is_same<T, float>::value || std::is_same<T, double>::value, "False type" );
 
     T avg = 0;
-    if(vec.size())
-    {
+    if(vec.size()){
         for(auto it : vec)
             avg += it;
         avg /=vec.size();
@@ -27,10 +26,15 @@ void input(std::vector<T> &vec){
     std::cout << "Fill the vector<" << typeid(T).name()  << "> with values:" << std::endl;
 
     T tmp;
-    for(int i = 0; i < vec.size(); ++i)
-    {
+    for(int i = 0; i < vec.size(); ++i){
         std::cout << "[" << i << "]: ";
         std::cin >> tmp;
+        if(std::cin.fail()){
+            std::cin.clear();
+            std::string str;
+            std::getline(std::cin, str);
+            throw std::invalid_argument("Error: wrong type!");
+        }
         vec[i] = tmp;
     }
 }
@@ -43,13 +47,28 @@ int main(int, char**){
     std::vector<double> double_vec(8);
     std::vector<float> float_vec(8);
 
-    input(int_vec);
-    std::cout << "Average: " << average(int_vec) << std::endl;
+    try{
+        input(int_vec);
+    }catch(std::invalid_argument& x){
+        std::cout << x.what() << std::endl;
+    }
 
-    input(double_vec);
+    std::cout << "Average: " << average(int_vec) << std::endl;
+    
+
+    try{
+        input(double_vec);
+    }catch(std::invalid_argument& x){
+        std::cout << x.what() << std::endl;
+    }
     std::cout << "Average: " << average(double_vec) << std::endl;
 
-    input(float_vec);
+
+    try{
+        input(float_vec);
+    }catch(std::invalid_argument& x){
+        std::cout << x.what() << std::endl;
+    }
     std::cout<< "Average: " << average(float_vec) << std::endl;
 
     return 0;
